3368c39f58dd4c1b8e54d2e22e6fa6924cc927c8
Sahapov <pankot222@gmail.com>
2022-04-10 17:29:54 +0500

    add ignore vscode

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-def greet ():
-	print("Welcome to the Brain Games!")
+def greet():
+    print("Welcome to the Brain Games!")
+
 
 def main():
-	greet()
+    greet()
+
 
 if __name__ == "__main__":
-	main()
-
+    main()
