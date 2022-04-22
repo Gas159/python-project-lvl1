38768de584567acdc13db9799eef811e470f12fc
gas <pankot222@gmail.com>
2022-04-22 14:11:42 +0500

    add lvl 9

--- brain_games/games/progres.py
+++ brain_games/games/progres.py
@@ -6,7 +6,7 @@ game_task = "What number is missing in the progression?"
 def get_question():
     start = randint(1, 10)
     step = randint(2, 7)
-    length = randint(5, 10)
+    length = randint(6, 10)
     stop = step * length + start
     random_index_for_change = randint(0, length - 1)
 
