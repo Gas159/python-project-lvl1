53f8a681a2b69cae0b5c069dbbae6c6ac6c2bb58
gas <pankot222@gmail.com>
2022-05-04 01:33:51 +0500

    ref 3

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,18 +6,19 @@ GAME_TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def check_prime(question):
     div_num = question // 2
     answer = True
-    while div_num > 1:
+    while div_num > 3 and answer:
         if question % div_num == 0:
             answer = False
-            break
         div_num -= 1
     return answer
 
 
 def get_question_and_answer():
-    question = random.randint(3, 20)
-    if check_prime(question) is True:
+    question = random.randint(2, 20)
+    if check_prime(question):
         correct_answer = "yes"
     else:
         correct_answer = "no"
     return question, correct_answer
+
+
